1a53d04e8c796b614cd321d7a7a824ac0ce495c5
Nilesh Gule <vn_nilesh@yahoo.com>
2023-01-02 19:56:54 +0800

    Added initial version of Java code

---
+++ src/java/techtalks-producer/src/main/java/com/nileshgule/techtalksproducer/TechtalksProducerApplication.java
@@ -0,0 +1,13 @@
+package com.nileshgule.techtalksproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtalksProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtalksProducerApplication.class, args);
+	}
+
+}
