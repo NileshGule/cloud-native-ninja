56cf43a33c49799e4a626a7f43452a9cc230a046
Nilesh Gule <vn_nilesh@yahoo.com>
2023-01-29 12:06:09 +0800

    Added initial version of k8s manifest files

--- Powershell/var.ps1
+++ Powershell/var.ps1
@@ -1,5 +1,4 @@
 $currentWorkingDirectory = (Get-Location).Path | Split-Path -Parent
-$helmRootDirectory = Join-Path $currentWorkingDirectory "helm"
 $projectRootDirectory = Join-Path $currentWorkingDirectory "k8s"
 $techTalksProducerRootDirectory = Join-Path $projectRootDirectory "TechTalksProducer"
 $techTalksConsumerRootDirectory = Join-Path $projectRootDirectory "TechTalksConsumer"
